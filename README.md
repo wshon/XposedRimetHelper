1144fe174d7b85bd04c5f400c98da842b3014b26
Skogen <muumlover@live.com>
2021-04-28 22:19:10 +0800

    change to northkinghelper

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
-# XposedRimetHelper  
+# XposedNorthkinghelper
 
-Xposed 钉钉辅助模块，暂时实现模拟位置。理论上支持 4.2.0 以上所有版本。  
-“酷公司”是怎样的反正我是不知道，不过恶心的公司恶心的做法应该都是一样了，模拟位置让你在哪都能打卡。  
+Xposed 京北方辅助模块，暂时实现模拟位置。
 
 ## 设置界面  
 
-![00.png](https://raw.githubusercontent.com/wuxiaosu/XposedRimetHelper/master/screenshots/00.png)
+![00.png](https://raw.githubusercontent.com/wuxiaosu/XposedNorthkinghelper/master/screenshots/00.png)
 
 ## 下载  
 
-- release from [github](https://github.com/wuxiaosu/XposedRimetHelper/releases)  
-
-- ~~release from [酷安](https://www.coolapk.com/apk/180336)~~(被和谐了)  
+- release from [github](https://github.com/wuxiaosu/XposedNorthkinghelper/releases)
---
+++ northkinghelper/src/main/java/com/wshon/northkinghelper/hook/LocationHook.java
@@ -0,0 +1,117 @@
+package com.wshon.northkinghelper.hook;
+
+import android.annotation.SuppressLint;
+
+import com.wshon.northkinghelper.BuildConfig;
+import com.wuxiaosu.widget.SettingLabelView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+
+/**
+ * Created by su on 2018/3/15.
+ * 定位 hook
+ */
+
+public class LocationHook {
+
+    private static XSharedPreferences sXsp;
+
+    private static boolean sFakeLocation;
+    private static boolean sFakeLocationTime;
+    private static String sStartTime;
+    private static String sLatitude;
+    private static String sLongitude;
+
+    private static final List<String> LISTENER_CLASS = new ArrayList<>();
+
+    public static void hook(final ClassLoader classLoader) {
+        try {
+            final Class<?> aMapLocationClientClazz =
+                    XposedHelpers.findClass("com.amap.api.location.AMapLocationClient", classLoader);
+            XposedBridge.hookAllMethods(aMapLocationClientClazz, "setLocationListener", new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    super.beforeHookedMethod(param);
+                    if (param.args.length == 1) {
+                        Class<?> listenerClazz = param.args[0].getClass();
+                        if (!LISTENER_CLASS.contains(listenerClazz.getName())) {
+                            LISTENER_CLASS.add(listenerClazz.getName());
+                            XposedBridge.hookAllMethods(listenerClazz, "onLocationChanged", new XC_MethodHook() {
+                                @Override
+                                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                                    param.args[0] = fakeAMapLocationObject(param.args[0]);
+                                    super.beforeHookedMethod(param);
+                                }
+                            });
+                        }
+                    }
+                }
+            });
+        } catch (Error | Exception e) {
+            XposedBridge.log(e);
+        }
+    }
+
+    private static void reload() {
+        if (sXsp == null) {
+            sXsp = new XSharedPreferences(BuildConfig.APPLICATION_ID, SettingLabelView.DEFAULT_PREFERENCES_NAME);
+            sXsp.makeWorldReadable();
+        }
+        sXsp.reload();
+        sFakeLocation = sXsp.getBoolean("fake_location", false);
+        sFakeLocationTime = sXsp.getBoolean("fake_location_time", false);
+        sStartTime = sXsp.getString("location_start_time", "8:40");
+        sLatitude = sXsp.getString("latitude", "39.908692");
+        sLongitude = sXsp.getString("longitude", "116.397477");
+    }
+
+    private static Object fakeAMapLocationObject(Object object) {
+        reload();
+        if (sFakeLocation) {
+            if (!sFakeLocationTime || isAfterSetTime(sStartTime)) {
+                XposedHelpers.callMethod(object, "setLatitude", Double.valueOf(sLatitude));
+                XposedHelpers.callMethod(object, "setLongitude", Double.valueOf(sLongitude));
+            }
+        }
+        return object;
+    }
+
+    /**
+     * 当前时间在设置时间之后
+     *
+     * @param setTime 01:12
+     * @return
+     */
+    @SuppressLint("SimpleDateFormat")
+    private static boolean isAfterSetTime(String setTime) {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        Date date = null;
+        Date now = null;
+        try {
+            date = sdf.parse(setTime);
+        } catch (ParseException e) {
+            try {
+                date = sdf.parse("8:40");
+            } catch (ParseException ignored) {
+            }
+        }
+        Calendar calendar = Calendar.getInstance();
+        try {
+            now = sdf.parse(calendar.get(Calendar.HOUR_OF_DAY)
+                    + ":" + calendar.get(Calendar.MINUTE));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date.getTime() < now.getTime();
+    }
+}
